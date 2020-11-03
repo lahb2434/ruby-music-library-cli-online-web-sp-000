a184b68e4dfa0bbbd8e80ba872adc1022d3e8c35
Chris Houghton <chrishoughton2434@gmail.com>
2020-11-03 21:35:09 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -31,8 +31,6 @@ class MusicLibraryController
           list_songs_by_genre
         when 'play song'
           play_song
-        else
-          puts 'unknown input'
         end
     end
   end
